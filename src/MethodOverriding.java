2b0a2db071c1c4a17a7c6aaa0388b0fa41b92efe
Harikrishnan Santhosh <harikrishnansanthosh06@gmail.com>
2026-02-06 10:44:03 +0530

    imple : dev
    Override display method in Engineer class to print its class name and call the parent class's display method.

--- src/MethodOverriding.java
+++ src/MethodOverriding.java
@@ -1,15 +1,20 @@
 class Employee {
-   
+   void display(){
+      System.out.println("Name of class is Employee");
+   }
 }
 
 class Engineer extends Employee {
-    // TODO: Override display() method
-    // 1. Print "Name of class is Engineer"
-    // 2. Call super.display() to print the parent's message
+    void display(){
+      System.out.println("Name of class is Employee");
+      super.display;
+   }
+   
 }
 
 public class MethodOverriding {
-   
+   Engineer obj = new Engineer();
+   obj.display;
         // TODO: Create an Engineer object
         // TODO: Call the display() method
     
